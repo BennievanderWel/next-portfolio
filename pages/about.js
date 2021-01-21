35b2362edaedde0256315470ba73aeb467b31643
Bennie van der Wel <bvdwel@anwb.nl>
2021-01-21 13:59:16 +0100

    Added github info on about page

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,24 @@
+import { Component } from 'react';
 import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
 
-export default () => {
-  return (
-    <Layout title='About'>
-      <p>A javascript programmer</p>
-      <img src='/static/js-logo.png' height='200px' />
-    </Layout>
-  );
-};
+export default class About extends Component {
+  static async getInitialProps() {
+    const res = await fetch('http://api.github.com/users/bennievanderwel');
+    console.log(res);
+    const data = await res.json();
+
+    return { user: data };
+  }
+
+  render() {
+    const { user } = this.props;
+    return (
+      <Layout title='About'>
+        <p>{user.name}</p>
+        <p>{user.bio}</p>
+        <img src={user.avatar_url} alt='Bennie' height='200px' />
+      </Layout>
+    );
+  }
+}
